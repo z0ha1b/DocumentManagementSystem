5695d1d610ad7a2455bdf35d3249c16429869e25
Hamza <hkkh1441@gmail.com>
2023-11-08 15:25:34 -0800

    migration files

---
+++ services/DocumentManagement/DocumentManagement.Persistence/Migrations/20231108052725_IsMergedAdd-migration.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DocumentManagement.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class IsMergedAddmigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "BatchId",
+                table: "Documents",
+                type: "TEXT",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "TEXT");
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsMerged",
+                table: "Documents",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsMerged",
+                table: "Documents");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "BatchId",
+                table: "Documents",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "TEXT",
+                oldNullable: true);
+        }
+    }
+}
